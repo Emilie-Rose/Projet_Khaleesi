c495ea8435a6b3748540fe2db7d82389c34404e1
Emilie Molina <emilie.molina@live.fr>
2023-06-22 01:49:32 +0200

    maj des fichiers config

--- src/main/java/com/doranco/repository/ArticleRepository.java
+++ src/main/java/com/doranco/repository/ArticleRepository.java
@@ -1,10 +1,17 @@
 package com.doranco.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import com.doranco.entity.Article;
+import com.doranco.entity.Categorie;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.doranco.entity.Article;
+import java.util.List;
 
 @Repository
-public interface ArticleRepository extends CrudRepository<Article, Integer> {
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    Article findById(long id);
+    
+    List<Article> findByCategorie(Categorie categorie);
 }
+
