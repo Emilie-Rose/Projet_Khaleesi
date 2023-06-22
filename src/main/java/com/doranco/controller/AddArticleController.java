8a393295b2011ea0d3e73c4080791ba203fbd246
Nasratie <nasratie145@gmail.com>
2023-06-22 14:56:29 +0200

    ajout du formulaire d'ajout d'articles

--- src/main/java/com/doranco/controller/AddArticleController.java
+++ src/main/java/com/doranco/controller/AddArticleController.java
@@ -1,11 +1,18 @@
 package com.doranco.controller;
 
 import com.doranco.entity.Article;
+import com.doranco.entity.Utilisateur;
 import com.doranco.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
 @Controller
 public class AddArticleController {
     private final ArticleRepository articleRepository;
@@ -19,5 +26,17 @@ public class AddArticleController {
     public String showArticleForm(Model model) {
         model.addAttribute("article", new Article());
         return "article_form.html";
+    }
+    @PostMapping("/add-article")
+    public RedirectView addArticle(@ModelAttribute("article") @Validated Article article,
+                                     BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new RedirectView("/add-article?error");
+        }
+
+        Article savedArcticle = articleRepository.save(article);
+        else{
+            return new RedirectView("/gestion-achat.html");
+        }
     }
 }
